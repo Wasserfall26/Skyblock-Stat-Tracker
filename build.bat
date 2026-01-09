5fc611a3cc02a34a1e440c37989e3636d6d4acec
Wasserfall26 <sebastiankrammer05@gmail.com>
2026-01-09 19:01:45 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,4 +0,0 @@
-$env:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-8.0.472.8-hotspot"
-$env:PATH = "C:\Program Files\Eclipse Adoptium\jdk-8.0.472.8-hotspot\bin;$env:PATH"
-./gradlew clean build
-Read-Host "Press Enter to exit"
